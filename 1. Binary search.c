5ffe80aa5455d11c795487200e723e29ad33bd45
kruthibabu7204-rgb <kruthibabu7204@gmail.com>
2026-04-16 11:10:42 +0530

    Add files via upload

---
+++ 1. Binary search.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+int binarySearch(int arr[], int n, int key) {
+    int low = 0, high = n - 1;
+
+    while (low <= high) {
+        int mid = (low + high) / 2;
+
+        if (arr[mid] == key) {
+            return mid;
+        }
+        else if (arr[mid] < key) {
+            low = mid + 1;
+        }
+        else {
+            high = mid - 1;
+        }
+    }
+    return -1;
+}
+
+int main() {
+    int arr[] = {2, 4, 6, 8, 10, 12, 14};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    int key;
+
+    printf("Enter the element to search: ");
+    scanf("%d", &key);
+
+    int result = binarySearch(arr, n, key);
+
+    if (result != -1) {
+        printf("Element found at index %d\n", result);
+    } else {
+        printf("Element not found in the array\n");
+    }
+
+    return 0;
+}
